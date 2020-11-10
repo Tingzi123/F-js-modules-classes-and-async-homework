bfd0d5622ed100a6afc41cc7e68644ffd7558a0a
Tingzi123 <1044373556@qq.com>
2020-11-10 22:52:06 +0800

    feat fetch and async

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,30 @@
-export const getPoetry
+// export const getPoetry=function(){
+//     return fetch('https://v1.jinrishici.com/all.json',{
+//         method:"GET",
+//         headers:{
+//             "Accept":"application/json",
+//             "Content-type":"application/json"
+//         },
+//     })
+//     .then(res =>{
+//         if(res.ok){
+//             return res.json();
+//         }    
+//     })
+//     .then(data =>{
+//         return [data.origin, data.author, data.content];
+//     })
+//     .catch(e => {
+//         throw new Error("error");
+//     })
+// }
+
+export const getPoetry = async () => {
+    const res = await fetch('https://v1.jinrishici.com/all.json')
+    if(res.ok) {
+      const { origin, author, content } = await res.json();
+      return [origin, author, content];
+    } 
+
+    return new Error("error")
+  } 
